4ce74dfff13ec50abd13ef01874467fee72fe8a9
Emily Riederer <emilyriederer@gmail.com>
2024-02-03 17:11:57 -0600

    Create metrics-by-vol.sql

---
+++ metrics-by-vol.sql
@@ -0,0 +1,71 @@
+/*
+Metrics by Volunteer
+
+The goal of this script is to compute the number of relationships and contacts for each volunteer
+
+Currently, the following metrics are implemented:
+- number of relationships (total / neighbor / friend)
+
+This script still needs to be enhanced with the following metrics:
+- number of primary survey questions answered 
+*/
+
+with
+
+users as (
+
+  select 
+    id as id_reach_user,
+    name as nm_reach_user,
+    created_on as dt_joined_reach_user
+  from `reach-vote`.`reach_txpatel24_share`.`users`
+
+),
+
+relat as (
+
+  select 
+    user_id as id_reach_user,
+    relationship_type_id as id_relat_type,
+    source,
+    reach_id as id_reach_voter,
+    voterfile_van_id as id_van_voter
+  from `reach-vote`.`reach_txpatel24_share`.`relationships`
+  where status = 'active'
+    
+),
+
+relat_type as (
+
+  select
+    id as id_relat_type,
+    name as nm_relat_type, 
+    case
+      when name in ('Neighbor','Voter I canvassed') 
+      then 1 else 0 end as ind_relat_neighb,
+    case
+      when name in ('Self','Non-supporter')
+      then 1 else 0 end as ind_relat_exclude,
+    case
+      when name not in ('Self', 'Non-supporter','Neighbor','Voter I canvassed')
+      then 1 else 0 end as ind_relat_friend
+  from `reach-vote`.`reach_txpatel24_share`.`relationship_types`
+
+)
+
+select
+
+  nm_reach_user,
+  count(1) as n_relationships,
+  sum(ind_relat_neighb) as n_relat_neighbor,
+  sum(ind_relat_friend) as n_relat_friend
+
+from
+
+  users
+  left join relat using (id_reach_user)
+  left join relat_type using (id_relat_type)
+
+group by 1
+order by 2 desc
+;
