a6bbeae684e2a42a60a6c7855bf62bda61c3826a
Emily Riederer <emilyriederer@gmail.com>
2024-02-03 21:04:29 -0600

    Validate questions and responses

--- validation-reach.sql
+++ validation-reach.sql
@@ -34,3 +34,32 @@ from `reach-vote`.`reach_txpatel24_share`.`relationship_types`;
 -- see all options for names
 select distinct name
 from `reach-vote`.`reach_txpatel24_share`.`relationship_types`;
+
+-- QUESTIONS TABLE VALIDATION --
+
+-- validate that questions table is one row per distinct question
+-- this is TRUE
+select count(1), count(distinct id), count(distinct name), count(distinct display_name)
+from `reach-vote`.`reach_txpatel24_share`.`questions`;
+
+-- see all options for questions 
+-- the ID for the primary question (support) is 5W9GPQ6G
+select *
+from `reach-vote`.`reach_txpatel24_share`.`questions`;
+
+-- RESPONSES TABLE VALIDATION --
+
+-- validate that responses are unique to a question x user x voter combo
+-- this is NOT TRUE
+select count(1), count(distinct recorded_by_user_id || ' ' || reach_id || ' ' || question_id)
+from `reach-vote`.`reach_txpatel24_share`.`responses`;
+
+-- when are responses not uniquely identified by user id x voter id x question id?
+-- observe that this happens when someone perhaps changes their answer with a different updated-on date, 
+-- so we would need to dedupe on that
+select *
+from `reach-vote`.`reach_txpatel24_share`.`responses`
+qualify count(1) over (partition by recorded_by_user_id, reach_id, question_id) > 1
+order by recorded_by_user_id, reach_id, question_id, updated_on;
+
+
