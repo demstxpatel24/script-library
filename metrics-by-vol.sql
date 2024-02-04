bb475f31d412c2dbb77dd2c81910ef00d2109450
Emily Riederer <emilyriederer@gmail.com>
2024-02-03 21:03:55 -0600

    Add count of support survey to query

--- metrics-by-vol.sql
+++ metrics-by-vol.sql
@@ -5,16 +5,8 @@ The goal of this script is to compute the number of relationships and contacts f
 
 Currently, the following metrics are implemented:
 - number of relationships (total / self / neighbor / friend)
-
-This script still needs to be enhanced with the following metrics:
-- number of primary survey questions answered 
+- number of completed surveys on the question of candidate support
 */
-
-/*metrics by volunteer*/
--- neighbors (neighbor + voter I canvassed)
--- friends (all other relationships except self and non-supporter)
--- contacted
--- primary survey question 
 
 with
 
@@ -58,6 +50,18 @@ relat_type as (
       then 1 else 0 end as ind_relat_friend
   from `reach-vote`.`reach_txpatel24_share`.`relationship_types`
 
+),
+
+survey_support as (
+
+  select
+    recorded_by_user_id as id_reach_user,
+    reach_id as id_reach_voter,
+    1 as ind_response_support
+  from `reach-vote`.`reach_txpatel24_share`.`responses`
+  where question_id = '5W9GPQ6G'
+  qualify row_number() over (partition by recorded_by_user_id, reach_id, question_id order by updated_on desc) = 1
+
 )
 
 select
@@ -66,13 +70,15 @@ select
   count(distinct id_reach_voter) as n_relationships,
   sum(ind_relat_self) as n_relat_self,
   sum(ind_relat_neighb) as n_relat_neighbor,
-  sum(ind_relat_friend) as n_relat_friend
+  sum(ind_relat_friend) as n_relat_friend,
+  sum(ind_response_support) as n_response_support
 
 from
 
   users
   left join relat using (id_reach_user)
   left join relat_type using (id_relat_type)
+  left join survey_support using (id_reach_user, id_reach_voter)
 
 group by 1
 order by 2 desc
